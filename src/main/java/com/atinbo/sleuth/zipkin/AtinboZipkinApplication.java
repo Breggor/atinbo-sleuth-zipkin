559db3f421782c939dc2e1ad50ac25b4e0a24d2e
Breggor <breggor@163.com>
2019-08-03 16:50:54 +0800

    init

---
+++ src/main/java/com/atinbo/sleuth/zipkin/AtinboZipkinApplication.java
@@ -0,0 +1,14 @@
+package com.atinbo.sleuth.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import zipkin2.server.internal.EnableZipkinServer;
+
+@EnableZipkinServer
+@SpringCloudApplication
+public class AtinboZipkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtinboZipkinApplication.class, args);
+    }
+}
